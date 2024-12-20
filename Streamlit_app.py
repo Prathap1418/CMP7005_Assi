4b19346e2bff8d5eee8707731639fab0edc49653
Prathap Thevendiram <78465357+Prathap1418@users.noreply.github.com>
2024-12-20 12:19:32 +0000

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import joblib
 
 def main():
     st.set_page_config(page_title="Air Quality Analysis App", layout="wide")
